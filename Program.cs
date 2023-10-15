f23fdb4938381213b6033ecc22fb61da6f86d8d8
Francis Kamara <franciskamara@hotmail.co.uk>
2023-10-15 14:16:30 +0100

    gameRestart while loop added, added gameEndMessage when bal is returned and when user quits game, removed recursive Cons.Clear when asked to spin again
    gameRestart while loop added,
    Added gameEndMessage when balance is returned and when user quits game,
    Removed recursive Cons.Clear when asked to spin again

--- Program.cs
+++ Program.cs
@@ -7,149 +7,159 @@ class Program
     const int COL_NUMBER = 3;
     const int ROW_NUMBER = 3;
     const int RDM_NUMBER_TOP_END = 11;
-    const int WIN_AMOUNT = 3;
+    const int WIN_AMOUNT = 10;
 
     static void Main(string[] args)
     {
-        Console.WriteLine("   Slot Machine"); //Intro text
-        Console.WriteLine("*-*-*-*-*-*-*-*-*-*\n");
+        int balance;
+        string gameEndMessage = "Thanks for playing";
 
-        Console.Write("Insert coins: ");
-        int balance = int.Parse(Console.ReadLine());
-
-        int[,] slotMachine = new int[ROW_NUMBER, COL_NUMBER]; //2D Array with numbers for each slot
-        Random rng = new Random(); //Random generator
-
-        while (balance > 0)
+        bool startGame;
+        while (true)
         {
-            /*
-                   For future reference
-            int[,] slotMachine = { { 1, 2, 1 }, { 4, 0, 6 }, { 1, 8, 1 } };
-            slotMachine[0, 0] = 1;
-            slotMachine[0, 1] = 2;
-            slotMachine[0, 2] = 3;
-            */
 
-            balance = balance - 1;
+            Console.WriteLine("   Slot Machine"); //Intro text
+            Console.WriteLine("*-*-*-*-*-*-*-*-*-*\n");
 
-            int rowIndex;
-            int colIndex;
-            for (rowIndex = 0; rowIndex < ROW_NUMBER; rowIndex++) //Generate random numbers for Slot per row
+            Console.WriteLine("TEST SLOT - to add input options...");
+            Console.Write("Insert coins: ");
+            balance = int.Parse(Console.ReadLine());
+
+            int[,] slotMachine = new int[ROW_NUMBER, COL_NUMBER]; //2D Array with numbers for each slot
+            Random rng = new Random(); //Random generator
+
+            while (balance > 0)
             {
-                for (colIndex = 0; colIndex < COL_NUMBER; colIndex++) //Generate random numbers for Slot per column
-                {
-                    slotMachine[rowIndex, colIndex] = rng.Next(RDM_NUMBER_TOP_END); //Random generator for each slot in machine
-                }
-            }//end Slot loop
+                /*
+                       For future reference
+                int[,] slotMachine = { { 1, 2, 1 }, { 4, 0, 6 }, { 1, 8, 1 } };
+                slotMachine[0, 0] = 1;
+                slotMachine[0, 1] = 2;
+                slotMachine[0, 2] = 3;
+                */
 
+                balance = balance - 1;
 
-            //Print Slot numbers
-            for (rowIndex = 0; rowIndex < ROW_NUMBER; rowIndex++)
-            {
-                for (colIndex = 0; colIndex < COL_NUMBER; colIndex++)
+                int rowIndex;
+                int colIndex;
+                for (rowIndex = 0; rowIndex < ROW_NUMBER; rowIndex++) //Generate random numbers for Slot per row
                 {
-                    Console.Write(slotMachine[rowIndex, colIndex] + "\t"); //Print random numbers for slot
-                }
-                Console.WriteLine();
-            }//end Slot Print loop
-
-            //Winning Scenarios
-            for (rowIndex = 0; rowIndex < ROW_NUMBER; rowIndex++) //Row match
-            {
-                int rowCounter = 0;
-                for (colIndex = 0; colIndex < COL_NUMBER - 1; colIndex++)
-                {
-                    if (slotMachine[rowIndex, colIndex] == slotMachine[rowIndex, colIndex + 1])
+                    for (colIndex = 0; colIndex < COL_NUMBER; colIndex++) //Generate random numbers for Slot per column
                     {
-                        rowCounter += 1; //counter of matches increases
+                        slotMachine[rowIndex, colIndex] = rng.Next(RDM_NUMBER_TOP_END); //Random generator for each slot in machine
                     }
-                }
-                if (rowCounter == COL_NUMBER - 1) //Counting matching pair values
-                {
-                    Console.WriteLine("Row match");
-                    balance = balance + WIN_AMOUNT;
-                }
-            } //end Row match 
+                }//end Slot loop
 
-            for (colIndex = 0; colIndex < COL_NUMBER; colIndex++) //Column match
-            {
-                int colCounter = 0;
+
+                //Print Slot numbers
+                for (rowIndex = 0; rowIndex < ROW_NUMBER; rowIndex++)
+                {
+                    for (colIndex = 0; colIndex < COL_NUMBER; colIndex++)
+                    {
+                        Console.Write(slotMachine[rowIndex, colIndex] + "\t"); //Print random numbers for slot
+                    }
+                    Console.WriteLine();
+                }//end Slot Print loop
+
+                //Winning Scenarios
+                for (rowIndex = 0; rowIndex < ROW_NUMBER; rowIndex++) //Row match
+                {
+                    int rowCounter = 0;
+                    for (colIndex = 0; colIndex < COL_NUMBER - 1; colIndex++)
+                    {
+                        if (slotMachine[rowIndex, colIndex] == slotMachine[rowIndex, colIndex + 1])
+                        {
+                            rowCounter += 1; //counter of matches increases
+                        }
+                    }
+                    if (rowCounter == COL_NUMBER - 1) //Counting matching pair values
+                    {
+                        Console.WriteLine("Row match");
+                        balance = balance + WIN_AMOUNT;
+                    }
+                } //end Row match 
+
+                for (colIndex = 0; colIndex < COL_NUMBER; colIndex++) //Column match
+                {
+                    int colCounter = 0;
+                    for (rowIndex = 0; rowIndex < ROW_NUMBER - 1; rowIndex++)
+                    {
+                        if (slotMachine[rowIndex, colIndex] == slotMachine[rowIndex + 1, colIndex])
+                        {
+                            colCounter += 1; //counter of matches increases
+                        }
+                    }
+                    if (colCounter == ROW_NUMBER - 1) //Counting matching pair values 
+                    {
+                        Console.WriteLine("Column match");
+                        balance = balance + WIN_AMOUNT;
+                    }
+                }//end Column match 
+
+                int diagCounter = 0; //Diagonal match - top Left
+                for (int index = 0; index < ROW_NUMBER - 1; index++)
+                {
+                    if (slotMachine[index, index] == slotMachine[index + 1, index + 1])
+                    {
+                        diagCounter += 1;
+
+                    }
+                } //end Diagonal Top left match
+                if (diagCounter == ROW_NUMBER - 1)
+                {
+                    Console.WriteLine("Diagonal match");
+                    balance = balance + WIN_AMOUNT;
+                }//end Diagonal match
+
+                int diag2Counter = 0; //Diagonal match - top Right
                 for (rowIndex = 0; rowIndex < ROW_NUMBER - 1; rowIndex++)
                 {
-                    if (slotMachine[rowIndex, colIndex] == slotMachine[rowIndex + 1, colIndex])
+                    int colSpecial = (COL_NUMBER - 1) - rowIndex;
+                    if (slotMachine[rowIndex, colSpecial] == slotMachine[rowIndex + 1, colSpecial - 1])
                     {
-                        colCounter += 1; //counter of matches increases
+                        diag2Counter += 1;
                     }
                 }
-                if (colCounter == ROW_NUMBER - 1) //Counting matching pair values 
+                if (diagCounter == ROW_NUMBER - 1)
                 {
-                    Console.WriteLine("Column match");
+                    Console.WriteLine("Diagonal match 2");
                     balance = balance + WIN_AMOUNT;
                 }
-            }//end Column match 
 
-            int diagCounter = 0; //Diagonal match - top Left
-            for (int index = 0; index < ROW_NUMBER - 1; index++)
-            {
-                if (slotMachine[index, index] == slotMachine[index + 1, index + 1])
-                {
-                    diagCounter += 1;
+                Console.WriteLine($"Your balance is now: {balance}\n");
 
-                }
-            } //end Diagonal Top left match
-            if (diagCounter == ROW_NUMBER - 1)
-            {
-                Console.WriteLine("Diagonal match");
-                balance = balance + WIN_AMOUNT;
-            }//end Diagonal match
-
-            int diag2Counter = 0; //Diagonal match - top Right
-            for (rowIndex = 0; rowIndex < ROW_NUMBER - 1; rowIndex++)
-            {
-                int colSpecial = (COL_NUMBER - 1) - rowIndex;
-                if (slotMachine[rowIndex, colSpecial] == slotMachine[rowIndex + 1, colSpecial - 1])
-                {
-                    diag2Counter += 1;
-                }
-            }
-            if (diagCounter == ROW_NUMBER - 1)
-            {
-                Console.WriteLine("Diagonal match 2");
-                balance = balance + WIN_AMOUNT;
-            }
-
-            Console.WriteLine($"Your balance is now: {balance}\n");
-
-            //prompts user to Restart or Exit game
-            Console.WriteLine("Spin again? y / n");
-            char endGame = Console.ReadKey().KeyChar;
-            if (endGame == 'y')
-            {
+                Console.WriteLine("Spin again? y / n");//prompts user to Restart or Exit game
+                char endGame = Console.ReadKey().KeyChar;
                 Console.Clear();
-            }
-            else if (endGame == 'n')
+                if (endGame == 'y')
+                {
+                }
+                else
+                {
+                    Console.WriteLine($"Balance returned: {balance}");
+                    Console.WriteLine(gameEndMessage);
+                    return;
+                }
+            }//end balance while Loop
+
+            if (balance <= 0)
             {
-                Console.Clear();
-                Console.WriteLine($"Balance returned: {balance}");
+                Console.WriteLine("Game over. You ran out of money.");
+                Console.WriteLine($"Insert more money to play again? y / n");
+            }
+
+            char restartGame = Console.ReadKey().KeyChar;
+            Console.Clear();
+            if (restartGame == 'y')
+            {
+                startGame = true;
+            }
+            else
+            {
+                Console.WriteLine(gameEndMessage);
                 return;
             }
-        }//end while Loop
-        if (balance <= 0)
-        {
-            Console.WriteLine("Game over. You ran out of money.");
-            Console.WriteLine($"Insert more money to play again? y / n");
-        }
-        char restartGame = Console.ReadKey().KeyChar;
-        Console.Clear();
-
-        if (restartGame == 'y')
-        {
-            Main(args);
-        }
-        else if (restartGame == 'n')
-        {
-            Console.WriteLine("Thanks for playing.");
-        }
+        }//end startGame loop
 
     }//end Main args
 }//end class Program
